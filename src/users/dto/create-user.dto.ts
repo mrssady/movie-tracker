dc0f2daec957c90629ca8cd006a342b7386f5ffb
mrsady <sadabahmad444gmail.com>
2025-07-22 15:16:06 +0545

    updating user.service.ts

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,35 @@
-export class CreateUserDto {}
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+    @IsNotEmpty()
+    name: string;
+    
+    @IsString()
+    @IsNotEmpty()
+    email: string;
+    
+    @IsString()
+    @IsNotEmpty()
+    password: string;
+
+    @IsString()
+    @IsNotEmpty()
+    avatarUrl?: string;
+
+    @IsString()
+    @IsNotEmpty()
+    createdAt: Date;
+
+    @IsString()
+    @IsNotEmpty()
+    updatedAt: Date;
+
+    @IsString()
+    @IsNotEmpty()
+    userMovies: UserMovie[];
+
+    @IsString()
+    @IsNotEmpty()
+    customLists: CustomList[];
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException
+} from '@nestjs/common';
+import { PrismaService } from '@prisma/client';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
+  constructor(private readonly prisma: PrismaService) {}
+
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
