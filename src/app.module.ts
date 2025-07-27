4b68ea459414698e6a1b56d64c593c539d33b33a
mrsady <sadabahmad444gmail.com>
2025-07-27 23:03:51 +0545

    user crud oop done also prisma problem solved

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
-import { MovieModule } from './movie/movie.module';
-import { GenreModule } from './genre/genre.module';
-import { MoviegenreModule } from './moviegenre/moviegenre.module';
-import { WatchstatusModule } from './watchstatus/watchstatus.module';
-import { UsermovieModule } from './usermovie/usermovie.module';
-import { CustomlistModule } from './customlist/customlist.module';
-import { CustomlistmovieModule } from './customlistmovie/customlistmovie.module';
-import { UsersModule } from './users/users.module';
-import { MoviesModule } from './movies/movies.module';
-import { GenresModule } from './genres/genres.module';
-import { MovieGenresModule } from './movie_genres/movie_genres.module';
-import { WatchStatusesModule } from './watch_statuses/watch_statuses.module';
-import { UserMoviesModule } from './user_movies/user_movies.module';
-import { CustomListsModule } from './custom_lists/custom_lists.module';
-import { CustomListMoviesModule } from './custom_list_movies/custom_list_movies.module';
+import { User } from './users/entities/user.entity';
+import { UserMovie } from './user_movies/entities/user_movie.entity';
+import { WatchStatus } from './watch_statuses/entities/watch_status.entity';
+import { Movie } from './movies/entities/movie.entity';
+import { MovieGenre } from './movie_genres/entities/movie_genre.entity';
+import { Genre } from './genres/entities/genre.entity';
+import { CustomList } from './custom_lists/entities/custom_list.entity';
+import { CustomListMovie } from './custom_list_movies/entities/custom_list_movie.entity';
+import { PrismaService } from './prisma/prisma.service';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [UserModule, MovieModule, GenreModule, MoviegenreModule, WatchstatusModule, UsermovieModule, CustomlistModule, CustomlistmovieModule, UsersModule, MoviesModule, GenresModule, MovieGenresModule, WatchStatusesModule, UserMoviesModule, CustomListsModule, CustomListMoviesModule],
+  imports: [
+    User,
+    UserMovie,
+    WatchStatus,
+    Movie,
+    MovieGenre,
+    Genre,
+    CustomList,
+    CustomListMovie,
+    PrismaModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -11,6 +11,7 @@ import { UpdateUserDto } from './dto/update-user.dto';
 export class UsersService {
   constructor(private readonly prisma: PrismaService) {}
 
+<<<<<<< HEAD
   async create(createUserDto: CreateUserDto) {
     let user = await this.prisma.user.findUnique({
       where: {phone: createUserDto.email },
@@ -49,3 +50,25 @@ export class UsersService {
   return users;
 }
 }
+=======
+  create(createUserDto: CreateUserDto) {
+    return 'This action adds a new user';
+  }
+
+  findAll() {
+    return `This action returns all users`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
+>>>>>>> 4fd2de4 (prisma problem solved also app.module problem)
